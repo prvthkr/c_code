440bb607fab69666eeee3293d220484a4ac9cb4d
prvthkr <31398919+prvthkr@users.noreply.github.com>
2023-02-10 14:32:26 +0530

    Create valid_para.c

--- valid_para.c
+++ valid_para.c
@@ -3,21 +3,36 @@
 #include <stdbool.h>
 
 bool valid_par(char *str) {
-    bool small_par = false;
-    bool mid_par = false;
-    bool big_par = false;
+    int small_par = 0;
+    int mid_par = 0;
+    int big_par = 0;
     
     for (int i = 0; str[i]; i++) {
-        if (str[i] == '(' || str[i] == ')') {
-            small_par = !small_par;
-        } else if (str[i] == '{' || str[i] == '}') {
-            mid_par = !mid_par;
-        } else if (str[i] == '[' || str[i] == '}') {
-            big_par = !big_par;
+        if (str[i] == '(') {
+            small_par++;
+        } else if (str[i] == '{') {
+            mid_par++;
+        } else if (str[i] == '[') {
+            big_par++;
+        } else if(str[i] == ')') {
+            if (small_par == 0) {
+                return false;
+            }
+            small_par--;
+        } else if(str[i] == '}') {
+            if (mid_par == 0) {
+                return false;
+            }
+            mid_par--;
+        } else if(str[i] == ']') {
+            if (big_par == 0) {
+                return false;
+            }
+            big_par--;
         }
     }
     
-    if (small_par == false && mid_par == false && big_par == false) {
+    if (small_par == 0 && mid_par == 0 && big_par == 0) {
         return true;
     }
     
